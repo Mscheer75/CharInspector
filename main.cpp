cc87e87e2d0e51f9c39495a5a83c8b067c9f5e24
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-02-08 11:25:38 -0800

    added the code to make the program function

--- main.cpp
+++ main.cpp
@@ -1,24 +1,33 @@
-//Author:
+/*Author:Matthew Scheer
+Program: char inspector, inspects characters
+Date: 2/8/2018
+*/
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-
+  char letter;
   cout<<"What character do you want to know about?\n";
-
-  //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
-
-  //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
-
-  //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
-  //no matter what they enter...
-  cout<<"The ASCII value is: ";
-
+  cin>>letter;
+ 
+ if(letter>='A' && letter<='Z')
+ {
+     //when user's entry is between A-Z...
+     cout<<" is an upper case letter!\n";
+ }if(letter>='a' && letter<='z')
+ {
+     //when user's entry is between a-z...
+     cout<<" is a lower case letter!\n";
+ }else
+ {
+     //in all other cases...
+     cout<<"?! Pssh. What are you talking about?\n";
+  }
+     //no matter what they enter...
+     cout<<"The ASCII value is: ";
+     cout<<(int)letter;
+     cout<<endl;
   return 0;
 }
